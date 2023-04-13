1b3c33111b75015722986e9924b7ccd43aa1a7f3
Ryan Keathley <keathley.joseph.ryan@gmail.com>
2023-04-12 23:02:05 -0700

    working minimal example

--- main.c
+++ main.c
@@ -1,4 +1,19 @@
-#include "src/lib.h"
+#include "src/lib.h"openIO
+
+char *Outfile = NULL;
+
+int openIO(void) {
+  if (!lrs_init("*lrsnash:"))
+    return FALSE;
+  fprintf(stderr, "\n");
+	if(Outfile != NULL) {
+	  if ((lrs_ofp = fopen(Outfile, "w")) == NULL) {
+	    fprintf(stderr, "\nBad output file name\n");
+	    return FALSE;
+	  }
+	}
+	return TRUE;
+}
 
 int solve_game(int rows, int cols)
 {
@@ -9,23 +24,24 @@ int solve_game(int rows, int cols)
 
 	// SUCKER
 	Game.payoff[0][0][0].num = 1; Game.payoff[0][0][0].den = 2;
-	Game.payoff[0][0][1].num = 1; Game.payoff[0][0][1].den = 1;
 	Game.payoff[0][1][0].num = 1; Game.payoff[0][1][0].den = 1;
-	Game.payoff[0][1][1].num = 0; Game.payoff[0][1][1].den = 1;
-
-	Game.payoff[1][0][0].num = 1; Game.payoff[1][0][0].den = 2;
-	Game.payoff[1][0][1].num = 0; Game.payoff[1][0][1].den = 1;
+	Game.payoff[1][0][0].num = 1; Game.payoff[1][0][0].den = 1;
 	Game.payoff[1][1][0].num = 0; Game.payoff[1][1][0].den = 1;
+
+	Game.payoff[0][0][1].num = 1; Game.payoff[0][0][1].den = 2;
+	Game.payoff[0][1][1].num = 0; Game.payoff[0][1][1].den = 1;
+	Game.payoff[1][0][1].num = 0; Game.payoff[1][0][1].den = 1;
 	Game.payoff[1][1][1].num = 1; Game.payoff[1][1][1].den = 1;
 
 	// printGame(&Game);
-	// lrs_solve_nash(&Game);
+	lrs_solve_nash(&Game);
 
 	return 0;
 }
 
 int main()
 {
+	int open = openIO();
 	solve_game(0, 0);
 	return 0;
 }
