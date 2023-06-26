3b7a4ca5400f98905a6210c41ba807d1d4a33fc8
Ryan Keathley <keathley.joseph.ryan@gmail.com>
2023-06-26 10:31:03 -0700

    gmp impl

--- src/lib-gmp.c
+++ src/lib-gmp.c
@@ -54,7 +54,7 @@ int lrs_solve_nash_(game *g, lrs_mp_vector row_data, lrs_mp_vector col_data)
     long numequilib = 0; /* number of nash equilibria found                      */
     long oldnum = 0;
 
-    Q1 = lrs_alloc_dat("LRS globals"); /* allocate and init structure for problem data */
+    Q1 = lrs_alloc_dat("LRS globals"); /* allocate and init structure for static problem data */
     if (Q1 == NULL)
     {
         return 0;
@@ -66,8 +66,6 @@ int lrs_solve_nash_(game *g, lrs_mp_vector row_data, lrs_mp_vector col_data)
 
     Q1->debug = Debug_flag;
     Q1->verbose = Verbose_flag;
-
-    // lrs_mp_vector col_data_copy = lrs_alloc_mp_vector(Q1->n + Q1->m);
 
     P1 = lrs_alloc_dic(Q1); /* allocate and initialize lrs_dic */
     if (P1 == NULL)
@@ -109,7 +107,7 @@ int lrs_solve_nash_(game *g, lrs_mp_vector row_data, lrs_mp_vector col_data)
         return 1;
 
     if (Q1->homogeneous && Q1->hull)
-        startcol++; /* col zero not treated as redundant   */
+        startcol++;
     col = startcol;
 
     do
@@ -117,14 +115,13 @@ int lrs_solve_nash_(game *g, lrs_mp_vector row_data, lrs_mp_vector col_data)
         prune = lrs_checkbound(P1, Q1);
         if (!prune && lrs_getsolution(P1, Q1, output1, col))
         {
-
             oldnum = numequilib;
 
             lrs_dic *P2;       /* This can get resized, cached etc. Loaded from P2orig */
             lrs_mp_matrix Lin; /* holds input linearities if any are found             */
             long col;          /* output column index for dictionary                   */
             long startcol = 0;
-            long prune = FALSE;
+            long prune = FALSE; /* if TRUE, getnextbasis will prune tree and backtrack  */
             long nlinearity;
             long *linearity;
 
@@ -155,6 +152,7 @@ int lrs_solve_nash_(game *g, lrs_mp_vector row_data, lrs_mp_vector col_data)
 
             if (!lrs_getfirstbasis2(&P2, Q2, P2orig, &Lin, TRUE, linindex))
                 goto sayonara;
+
             if (Q2->homogeneous && Q2->hull)
                 startcol++;
 
@@ -164,12 +162,10 @@ int lrs_solve_nash_(game *g, lrs_mp_vector row_data, lrs_mp_vector col_data)
                 col = 0;
                 if (!prune && lrs_getsolution(P2, Q2, output2, col))
                 {
-                    if (Q2->verbose)
-                        prat_(" \np1's obj value: ", P2->objnum, P2->objden);
-
                     long lrs_nashoutput1 = TRUE;
                     long i1;
                     long origin = TRUE;
+
                     for (i1 = 1; i1 < Q2->n; i1++)
                         if (!__zero(output2[i1]))
                             origin = FALSE;
@@ -184,7 +180,7 @@ int lrs_solve_nash_(game *g, lrs_mp_vector row_data, lrs_mp_vector col_data)
 
                         for (i1 = 0; i1 < Q2->n; i1++)
                         {
-                            *(col_data[i1]) = *output2[i1];
+                            __copy(col_data[i1], output2[i1]);
                         }
 
                         numequilib++;
@@ -197,13 +193,10 @@ int lrs_solve_nash_(game *g, lrs_mp_vector row_data, lrs_mp_vector col_data)
 
             if (numequilib > oldnum || Q1->verbose)
             {
-                if (Q1->verbose)
-                    prat_(" \np2's obj value: ", P1->objnum, P1->objden);
-
                 long lrs_nashoutput2 = TRUE;
                 long i1;
                 long origin = TRUE;
-                /* do not print the origin for either player */
+
                 for (i1 = 1; i1 < Q1->n; i1++)
                     if (!__zero(output1[i1]))
                         origin = FALSE;
@@ -213,13 +206,12 @@ int lrs_solve_nash_(game *g, lrs_mp_vector row_data, lrs_mp_vector col_data)
 
                 if (lrs_nashoutput2)
                 {
-                    // fprintf(lrs_ofp, "%ld ", 1L);
                     for (i1 = 1; i1 < Q1->n; i1++)
                         prat_("", output1[i1], output1[0]);
 
                     for (i1 = 0; i1 < Q1->n; i1++)
                     {
-                        *(row_data[i1]) = *output1[i1];
+                        __copy(row_data[i1], output1[i1]);
                     }
                     goto done;
                 }
@@ -228,14 +220,12 @@ int lrs_solve_nash_(game *g, lrs_mp_vector row_data, lrs_mp_vector col_data)
     } while (lrs_getnextbasis(&P1, Q1, prune));
 
 done:
-
     lrs_clear_mp_vector(output1, Q1->m + Q1->n);
     lrs_clear_mp_vector(output2, Q1->m + Q1->n);
 
     lrs_free_dic(P1, Q1); /* deallocate lrs_dic */
     lrs_free_dat(Q1);     /* deallocate lrs_dat */
 
-    /* 2015.10.10  new code to clear P2orig */
     Q2->Qhead = P2orig; /* reset this or you crash free_dic */
     P2orig->A = A2orig; /* reset this or you crash free_dic */
 
@@ -243,15 +233,17 @@ done:
     lrs_free_dat(Q2);         /* deallocate lrs_dat */
 
     free(linindex);
-
     return 0;
 }
 
 void prat_(const char *name, lrs_mp Nin, lrs_mp Din)
-/*print the long precision rational Nt/Dt  */
 {
-    lrs_mp Nt, Dt;
-    __copy(Nt, Nin);
-    __copy(Dt, Din);
-    reduce(Nt, Dt);
+    lrs_mp temp1, temp2;
+    lrs_alloc_mp(temp1);
+    lrs_alloc_mp(temp2);
+    __copy(temp1, Nin);
+    __copy(temp2, Din);
+    reduce(temp1, temp2);
+    lrs_clear_mp(temp1);
+    lrs_clear_mp(temp2);
 }
